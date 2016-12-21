21e7a0bec561ad45e97ff5ac2d44d8e2f7de7cc6
Patricia Disaign <patriciandrea.p@gmail.com>
2016-12-21 13:05:56 -0300

    Cambios y detalles portafolio, se agregó menú

--- js/script.js
+++ js/script.js
@@ -38,64 +38,8 @@ $(document).ready(function() {
   $(window).scroll(function(e) {
     $('.parallax').each(parallax);
   });
-
-
-  $('#btnEnviar').click(function(){
-    estado=0;
-    var nombre=$('input[name="nombre"]').val();
-    var correo=$('input[name="correo"]').val();
-    var mensaje=$('textarea[name="mensaje"]').val();
-
-    console.log(nombre);
-    console.log(correo);
-    console.log(mensaje);
-
-
-    if(nombre.length<2){
-      $('input[name="nombre"]').css('border-color','red');    
-    }else{
-      estado++;
-      $('input[name="nombre"]').css('border-color','#2199e8');        
-    }
-
-    if(mensaje.length<3){
-      $('textarea[name="mensaje"]').css('border-color','red');    
-    }else{
-      estado++;
-      $('textarea[name="mensaje"]').css('border-color','#2199e8');    
-    }
-    
-    validacion = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-        if((correo.length<3) || (!validacion.test(correo))){
-            $('input[name="correo"]').css('border-color','red');    
-        }else{
-            estado++;
-            $('input[name="correo"]').css('border-color','#2199e8');    
-        }
-    $('#formContacto').submit(function(e) {
-            if (ok != 3) {
-                e.preventDefault();
-                $('#formContacto .success').css('display','none');
-                $('#formContacto .alert').css('display','block');
-            }else{
-                $.ajax({
-                    type: 'POST',
-                    url: 'envio-email.php',
-                    data: $(this).serialize(),
-                    success: function(data) {
-                        $('#formContacto .success').css('display','none');
-                        $('#formContacto .alert').css('display','block');
-                    }
-                });
-                e.preventDefault();
-                $('#formContacto').each(function(){
-                    $(this).each (function() { this.reset(); });
-                })
-                setTimeout(function() {
-                    $("#formContacto .success").fadeOut(1500);
-                },2500);
-                $('#formContacto .success').css('display','none');
-            }
-        });
+    // Efecto portafolio
+  $(".entry-wrapper").hover(function(){
+    $("this .entry-content").fadeIn('slow')
   });
 }); 
